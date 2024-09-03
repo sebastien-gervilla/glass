9d51775ed0df7306fd62113b51f34d7fa6711447
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2024-09-03 11:57:02 +0200

    feat: Added parseIntergerLiteral method

--- language/parser/parser.go
+++ language/parser/parser.go
@@ -213,6 +213,23 @@ func (parser *Parser) parseIdentifier() ast.Expression {
 	}
 }
 
+func (parser *Parser) parseIntegerLiteral() ast.Expression {
+	literal := &ast.IntegerLiteral{
+		Token: parser.currentToken,
+	}
+
+	value, conversionError := strconv.ParseInt(parser.currentToken.Literal, 0, 64)
+	if conversionError != nil {
+		message := fmt.Sprintf("could not parse %q as integer", parser.currentToken.Literal)
+		parser.errors = append(parser.errors, message)
+		return nil
+	}
+
+	literal.Value = value
+	return literal
+}
+
+
 // Utils
 
 func (parser *Parser) isCurrentToken(token token.TokenType) bool {
