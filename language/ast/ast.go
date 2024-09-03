7428082ae04bbd06df74c0d5183ca63aa2528539
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2024-09-03 11:49:06 +0200

    feat: Added Boolean AST representation

--- language/ast/ast.go
+++ language/ast/ast.go
@@ -177,3 +177,14 @@ func (expression *InfixExpression) String() string {
 	return buffer.String()
 }
 
+// Boolean
+type Boolean struct {
+	Token token.Token
+	Value bool
+}
+
+func (boolean *Boolean) expressionNode()      {}
+func (boolean *Boolean) TokenLiteral() string { return boolean.Token.Literal }
+func (boolean *Boolean) String() string       { return boolean.Token.Literal }
+
+
