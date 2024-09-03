bd71e6a1132ce1bb04ece3ace570b16336bd6f01
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2024-09-03 16:28:19 +0200

    feat: Added basic environment

--- language/object/object.go
+++ language/object/object.go
@@ -25,6 +25,26 @@ type Error struct {
 func (e *Error) GetType() ObjectType { return ERROR_OBJECT }
 func (e *Error) Inspect() string     { return "ERROR: " + e.Message }
 
+// Environment
+type Environment struct {
+	store map[string]Object
+}
+
+func NewEnvironment() *Environment {
+	store := make(map[string]Object)
+	return &Environment{store: store}
+}
+
+func (environment *Environment) Get(name string) (Object, bool) {
+	obj, ok := environment.store[name]
+	return obj, ok
+}
+
+func (environment *Environment) Set(name string, val Object) Object {
+	environment.store[name] = val
+	return val
+}
+
 // Integers
 type Integer struct {
 	Value int64
