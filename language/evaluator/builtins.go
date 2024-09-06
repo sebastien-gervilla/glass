fb5ae4e4ce2e6f60cc17b88d0e445674bcfcb2d9
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2024-09-06 15:12:32 +0200

    imp: Changed print not to jump line for each argument

--- language/evaluator/builtins.go
+++ language/evaluator/builtins.go
@@ -9,7 +9,7 @@ var builtins = map[string]*object.Builtin{
 	"print": {
 		Function: func(arguments ...object.Object) object.Object {
 			for _, argument := range arguments {
-				fmt.Println(argument.Inspect())
+				fmt.Print(argument.Inspect())
 			}
 
 			return NULL
